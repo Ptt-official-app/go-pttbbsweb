a0e028132b9ffa56b603c2a13e0fd4d439021f0a
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2020-12-16 07:04:19 -0500

    add schema definition for user, board, article, comment

---
+++ schema/article.go
@@ -0,0 +1,34 @@
+package schema
+
+import (
+	"github.com/Ptt-official-app/go-openbbsmiddleware/types"
+	"github.com/Ptt-official-app/go-pttbbs/bbs"
+	"github.com/Ptt-official-app/go-pttbbs/ptttype"
+	"github.com/Ptt-official-app/go-pttbbs/types/proto"
+)
+
+type Article struct {
+	BBoardID   bbs.BBoardID     `bson:"bid"`
+	ArticleID  bbs.ArticleID    `bson:"aid"`
+	IsDeleted  bool             `bson:"deleted"`
+	Filename   string           `bson:"filename"`
+	CreateTime types.Time8      `bson:"create_time_ts"`
+	MTime      types.Time8      `bson:"mtime_ts"`
+	Recommend  int              `bson:"recommend"`
+	Owner      string           `bson:"owner"`
+	Date       string           `bson:"date"`
+	Title      string           `bson:"title"`
+	Money      int              `bson:"money"`
+	Type       string           `bson:"type"`
+	Class      string           `bson:"class"`
+	Filemode   ptttype.FileMode `bson:"mode"`
+	URL        string           `bson:"url"`
+	Read       bool             `bson:"read"`
+
+	Content proto.Content `bson:"content"`
+	IP      string        `bson:"ip"`
+	Country string        `bson:"country"`
+	BBS     string        `bson:"bbs"`
+
+	UpdateNanoTS types.NanoTS `bson:"update_nano_ts"`
+}
---
+++ schema/board.go
@@ -0,0 +1,42 @@
+package schema
+
+import (
+	"github.com/Ptt-official-app/go-openbbsmiddleware/types"
+	"github.com/Ptt-official-app/go-pttbbs/bbs"
+	"github.com/Ptt-official-app/go-pttbbs/ptttype"
+)
+
+type Board struct {
+	BBoardID  bbs.BBoardID    `bson:"bid"`
+	Brdname   string          `bson:"brdname"`
+	Title     string          `bson:"title"`
+	BrdAttr   ptttype.BrdAttr `bson:"flag"`
+	BoardType string          `bson:"the_type"`
+	Category  string          `bson:"class"`
+	//NUser     int             `bson:"nuser"`  /* use db-count to get current #users */
+	BMs   []string `bson:"bms"`
+	Total int      `bson:"total"` /* 需要即時知道. 因為 read 頻率高. 並且跟 last-post-time-ts 一樣 write 頻率 << read 頻率 */
+
+	LastPostTimeTS types.Time8 `bson:"last_post_time_ts"` /* 需要即時知道來做板的已讀 */
+
+	UpdateTimeTS types.Time8 `bson:"update_time_ts"` /* XXX 不知 c-pttbbs 對於這個的定義 */
+
+	VoteLimitLogins  int `bson:"vote_limit_logins"`
+	PostLimitLogins  int `bson:"post_limit_logins"`
+	VoteLimitBadpost int `bson:"vote_limit_bad_post"`
+	PostLimitBadpost int `bson:"post_limit_bad_post"`
+
+	//NVote             int         `bson:"vote"` /* use db-count to get current #vote */
+	VoteClosingTimeTS types.Time8 `bson:"vtime_ts"`
+
+	Level                ptttype.PERM `bson:"perm"`
+	LastSetTimeTS        types.Time8  `bson:"last_set_time_ts"`
+	PostExpire           int          `bson:"post_expire"`
+	EndGambleTS          types.Time8  `bson:"end_gamble_ts"`
+	PostType             string       `bson:"post_type"`
+	FastRecommendPauseTS types.Time8  `bson:"fast_recommend_pause_ts"`
+
+	IsDeleted bool `bson:"deleted"`
+
+	UpdateNanoTS types.NanoTS `bson:"update_nano_ts"`
+}
