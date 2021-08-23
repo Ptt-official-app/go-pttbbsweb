eb6077441e2a05d719e11db2b55bf2d1164a2a22
tingyuchang <tingyugenius@gmail.com>
2021-08-23 10:54:15 +0800

    add test for TOURL()

---
+++ apitypes/utils_test.go
@@ -0,0 +1,42 @@
+package apitypes
+
+import (
+	"github.com/Ptt-official-app/go-openbbsmiddleware/types"
+	"testing"
+)
+
+func TestToURL(t *testing.T) {
+	type args struct {
+		fbboardID  FBoardID
+		farticleID FArticleID
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "should be {URL}/board/Baseball/article/M.1629685889.A.AEB",
+			args: args{fbboardID: "Baseball", farticleID: "M.1629685889.A.AEB"},
+			want: types.URL_PREFIX+"/board/Baseball/article/M.1629685889.A.AEB",
+		},
+		{
+			name: "null board should return empty string",
+			args: args{fbboardID: "", farticleID: "M.1629685889.A.AEB"},
+			want: "",
+		},
+		{
+			name: "null article should return empty string",
+			args: args{fbboardID: "Baseball", farticleID: ""},
+			want: "",
+		},
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ToURL(tt.args.fbboardID, tt.args.farticleID); got != tt.want {
+				t.Errorf("ToURL() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
