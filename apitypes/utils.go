7f46a29be7c91482ab14ddf51dc93559efd8b084
tingyuchang <tingyugenius@gmail.com>
2021-08-23 10:51:27 +0800

    Remove ALLPOST issue handling discuss: https://github.com/Ptt-official-app/go-openbbsmiddleware/issues/220\#issuecomment-903392529

--- apitypes/utils.go
+++ apitypes/utils.go
@@ -1,32 +1,12 @@
 package apitypes
 
 import (
-	"fmt"
 	"github.com/Ptt-official-app/go-openbbsmiddleware/types"
-	"github.com/Ptt-official-app/go-openbbsmiddleware/schema"
-	"github.com/Ptt-official-app/go-pttbbs/bbs"
-	"strings"
 )
 
 func ToURL(fbboardID FBoardID, farticleID FArticleID) string {
-	// ALLPOST need to translate to original board
-	if fbboardID == "ALLPOST" {
-		articleID := farticleID.ToArticleID()
-		articleSummaries, err := schema.GetArticleSummariesByArticleIDs([]bbs.ArticleID{articleID})
-		if err != nil {
-			//TODO error handling
-			fmt.Println(err)
-			return ""
-		}
-		//TODO if not find?
-		for _, v := range articleSummaries {
-			b := strings.Join(strings.Split(string(v.BBoardID), "_")[1:], "")
-			if b == "ALLPOST" {
-				continue
-			}
-			return types.FRONTEND_PREFIX + types.API_PREFIX + "/" + "board" + "/" + b + "/" + "article" + "/" + string(farticleID)
-		}
+	if fbboardID == "" || farticleID == "" {
 		return ""
 	}
-	return types.FRONTEND_PREFIX + types.API_PREFIX + "/" + "board" + "/" + string(fbboardID) + "/" + "article" + "/" + string(farticleID)
+	return types.URL_PREFIX + "/" + "board" + "/" + string(fbboardID) + "/" + "article" + "/" + string(farticleID)
 }
