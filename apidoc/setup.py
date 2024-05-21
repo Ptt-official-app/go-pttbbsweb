38fcb550653b26d230ae4f8583d5f46940cd7650
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2024-05-20 20:17:23 -0400

    update token-user in apidoc

--- apidoc/setup.py
+++ apidoc/setup.py
@@ -4,8 +4,8 @@ setuptools.setup(
     name="apidoc",
     version='0.0.1',
     install_requires=[
-        "pyyaml==5.3.1",
-        "Flask==1.1.2",
+        "pyyaml==6.0.1",
+        "Flask==3.0.3",
         "flask-swagger @ git+https://github.com/chhsiao1981/flask-swagger.git@flaskswagger-with-from-file-keyword#egg=flask-swagger",
     ],
     classifiers=[
