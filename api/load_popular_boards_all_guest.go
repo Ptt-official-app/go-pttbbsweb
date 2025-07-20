e8d813335666186fc031e4b856991a7a8815d26e
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-07-20 05:46:47 -0400

    all-guest: add board-stat as NBRD_BOARD in api.LoadPopularBoardsAllGuest.

--- api/load_popular_boards_all_guest.go
+++ api/load_popular_boards_all_guest.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/Ptt-official-app/go-pttbbs/bbs"
+	"github.com/Ptt-official-app/go-pttbbs/ptttype"
 	"github.com/Ptt-official-app/go-pttbbsweb/apitypes"
 	"github.com/Ptt-official-app/go-pttbbsweb/schema"
 	"github.com/gin-gonic/gin"
@@ -19,7 +20,9 @@ func LoadPopularBoardsAllGuest(remoteAddr string, params interface{}, c *gin.Con
 
 	userBoardInfoMap := make(map[bbs.BBoardID]*apitypes.UserBoardInfo)
 	for _, board := range boardSummaries_db {
-		userBoardInfoMap[board.BBoardID] = &apitypes.UserBoardInfo{}
+		userBoardInfoMap[board.BBoardID] = &apitypes.UserBoardInfo{
+			Stat: ptttype.NBRD_BOARD,
+		}
 	}
 
 	result = NewLoadPopularBoardsResult(boardSummaries_db, userBoardInfoMap, "guest")
